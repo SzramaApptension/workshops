a253663ab7980909de71f2b3fa72a7109823b6df
Rafal Szrama <rszrama@apptension.com>
2024-05-16 19:15:24 +0200

    Revert example file

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -18,10 +18,3 @@ test("get started link", async ({ page }) => {
     page.getByRole("heading", { name: "Installation" }),
   ).toBeVisible();
 });
-
-test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
